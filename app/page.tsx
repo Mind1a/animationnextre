61272c5ea8cace4cc50ceb6964abcd8ab85ed1e0
Mindia Arabuli <65043119+Mind1a@users.noreply.github.com>
2024-11-08 21:09:37 +0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import HorizontalScrollCarousel from "@/components/HorizontalScrollCarousel";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="bg-neutral-800">
+      <section className="flex h-48 items-center justify-center min-h-screen">
+        <span className="font-semibold uppercase text-neutral-500">
+          Scroll down
+        </span>
+      </section>
+      <HorizontalScrollCarousel />
+      <section className="flex h-48 items-center justify-center min-h-screen">
+        <span className="font-semibold uppercase text-neutral-500">
+          Scroll Up
+        </span>
+      </section>
+    </main>
+  );
 }
---
+++ components/MyCard.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { CardType } from "@/constants/card";
+import Image from "next/image";
+
+export default function MyCard({ card }: { card: CardType }) {
+  return (
+    <div className="group h-[450px] w-[450px] overflow-hidden bg-neutral-200 flex flex-col items-center justify-center">
+      <Image
+        className="transition-transform duration-300 group-hover:scale-110"
+        src={card.image}
+        width={350}
+        height={150}
+        alt={card.title}
+      />
+    </div>
+  );
+}
